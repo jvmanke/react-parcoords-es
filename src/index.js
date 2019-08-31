46deb1dcc40b0ad42b2cd14e493fb15b91c1c26a
Joao Victor Manke <38359528+jvmanke@users.noreply.github.com>
2019-08-31 10:04:24 -0300

    Fix the proof of concept

--- src/index.js
+++ src/index.js
@@ -1,33 +1,31 @@
 import React, { useEffect, useRef } from "react"
 import ReactDOM from "react-dom"
 import Parcoords from "parcoord-es"
-// import "parcoord-es/dist/parcoords.css"
+import "parcoord-es/dist/parcoords.css"
 
-function Test({ data }) {
+function Test() {
   const chartRef = useRef(null)
   useEffect(() => {
     if (chartRef !== null) {
       // use default config;
-      const chart = Parcoords()(chartRef.current)
-        .data(data)
+      Parcoords()(chartRef.current)
+        .data([
+          [0, -0, 0, 0, 0, 1],
+          [1, -1, 1, 2, 1, 1],
+          [2, -2, 4, 4, 0.5, 1],
+          [3, -3, 9, 6, 0.33, 1],
+          [4, -4, 16, 8, 0.25, 1]
+        ])
         .render()
         .createAxes()
     }
   }, [chartRef])
 
   return (
-    <div>
-      <div style={{ height: `100px` }} ref={chartRef} className={"parcoords"} />
+    <div style={{ height: 300 }}>
+      <div style={{ height: `100%` }} ref={chartRef} className={"parcoords"} />
     </div>
   )
 }
 
-ReactDOM.render(
-  <>
-    <Test data={[[3, -3, 9, 6, 0.33, 1], [4, -4, 16, 8, 0.25, 1]]} />
-    <Test
-      data={[[0, -0, 0, 0, 0, 1], [1, -1, 1, 2, 1, 1], [2, -2, 4, 4, 0.5, 1]]}
-    />
-  </>,
-  document.querySelector("#root")
-)
+ReactDOM.render(<Test />, document.querySelector("#root"))
